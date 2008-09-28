e8189c4321df6f2670c35717c8858dc3a18afd3b
dan corson <danl@pima.(none)>
2008-09-27 18:51:05 -0700

    level and number options

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,62 @@
 module Main where
 
+import Control.Monad
 import Data.List
 import Data.Maybe
 import FUtil
 import HSH
+import System.Console.GetOpt
 import System.Directory
 import System.Environment
 import System.FilePath
 import Text.HTML.TagSoup
 import qualified Data.Set as Set
 
--- todo getopt
+data Options = Options {
+  optNum :: Int,
+  optLevel :: Int
+  }
+
+defaultOptions :: Options
+defaultOptions = Options {
+  optNum = 10,
+  optLevel = 10
+  }
+
+options :: [OptDescr (Options -> Options)]
+options = [
+  Option "n" ["num"]
+    (ReqArg (\ n o -> o {optNum = read n}) "NUM")
+    "do NUM problems (default 10)",
+  Option "l" ["level"]
+    (ReqArg (\ l o -> o {optLevel = read l}) "LEVEL")
+    "level (30 is 30k, 25, 20, 18, 15, 12, 10, 8,\n\
+    \7, .., 1, -1 is 1dan, -2, .. -6)"
+  ]
 
 levToDiff :: (Num t, Num t1) => t -> t1
-levToDiff 12 = 5
-levToDiff 10 = 6
+levToDiff n = case n of
+  30 -> 0
+  25 -> 1
+  20 -> 2
+  18 -> 3
+  15 -> 4
+  12 -> 5
+  8 -> 6
+  7 -> 7
+  6 -> 8
+  5 -> 9
+  4 -> 10
+  3 -> 11
+  2 -> 12
+  1 -> 13
+  -1 -> 14
+  -2 -> 15
+  -3 -> 16
+  -4 -> 17
+  -5 -> 18
+  -6 -> 19
+  _ -> error "unknown level"
 
 getTags :: Int -> IO [Tag]
 getTags lev = do
@@ -38,17 +80,25 @@ readFileStrict f = do
   c <- readFile f
   length c `seq` return c
 
+doErr :: String -> a
+doErr err = let
+  usage = "usage: dgp [options]"
+  in error $ err ++ usageInfo usage options
+
 main :: IO ()
 main = do
   args <- getArgs
   home <- getEnv "HOME"
   let
+    (optsPre, moreArgs, errs) = getOpt Permute options args
+    opts = foldl (flip id) defaultOptions optsPre
     dir = home </> ".dgp"
     cached = True
-    num = 10
-    lev = read . fromMaybe "10" $ listToMaybe args
+    lev = optLevel opts
     f = dir </> "lev." ++ show lev
     fDid = dir </> "did"
+  unless (null errs) . doErr $ concat errs
+  unless (null moreArgs) $ doErr "Unexpected arguments\n"
   unlessM (doesDirectoryExist dir) $ createDirectory dir
   cached' <- if cached then doesFileExist f else return False
   c <- if cached' then readFile f else do
@@ -76,4 +126,4 @@ main = do
       runIO ("ff", ["http://goproblems.com/prob.php3?id=" ++ prob])
   sequence_ . 
     intersperse (putStrLn "press enter when done.." >> getLine >> return ()) .
-    replicate num $ doOne c
+    replicate (optNum opts) $ doOne c
